800f5260d044ed72ff41817eec0e0b5031d99fea
FencingDudette333 <angelvickylearning@gmail.com>
2022-10-18 16:17:01 -0700

    Roman started to code the hardware map today!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectShortage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectShortage.java
@@ -1,16 +1,28 @@
 package org.firstinspires.ftc.teamcode.Projects;
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 
 //hardware map file
 public class ProjectShortage extends Project{
+    public DcMotor right = null;
+    public DcMotor left = null;
+
     @Override
     public void init(HardwareMap ahwMap) {
         hwMap = ahwMap;
 
         //Define and Initialize Motors
+        right = hwMap.dcMotor.get("right");
+        left = hwMap.dcMotor. get("left");
 
         //Setup Motor directions and Encoder settings
+        right.setDirection(DcMotor.Direction.FORWARD);
+        left.setDirection(DcMotor.Direction.REVERSE);
+
+        right.
 
         // Set all motors to zero power
 
