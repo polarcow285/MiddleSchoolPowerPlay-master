f50ade8de7f0852414017290d80a2d3f618cf0c4
FencingDudette333 <angelvickylearning@gmail.com>
2022-11-15 15:00:36 -0800

    I CODED stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectShortage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectShortage.java
@@ -7,8 +7,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 //hardware map file
 public class ProjectShortage extends Project{
-    public DcMotor right = null;
-    public DcMotor left = null;
+    public DcMotor fRight = null;
+    public DcMotor fLeft = null;
+    public DcMotor bRight = null;
+    public DcMotor bLeft = null;
     public DcMotor lift_lol = null;
     public DcMotor lift_mochi = null;
     public Servo clamp_lol = null;
@@ -18,29 +20,37 @@ public class ProjectShortage extends Project{
         hwMap = ahwMap;
 
 
-        left = hwMap.dcMotor.get("leftWheel");
-        right = hwMap.dcMotor.get("rightWheel");
+        fLeft = hwMap.dcMotor.get("fLeftWheel");
+        fRight = hwMap.dcMotor.get("fRightWheel");
+        bLeft = hwMap.dcMotor.get("bLeftWheel");
+        bRight = hwMap.dcMotor.get("bRightWheel");
         lift_lol = hwMap.dcMotor.get("liftLOL");
         lift_mochi = hwMap.dcMotor.get("liftMochi");
         clamp_lol = hwMap.servo.get("clampLOL");
         clamp_mochi = hwMap.servo.get("clampMochi");
 
         //motors face towards each other
-        right.setDirection(DcMotor.Direction.FORWARD);
-        left.setDirection(DcMotor.Direction.REVERSE);
+        fRight.setDirection(DcMotor.Direction.FORWARD);
+        fLeft.setDirection(DcMotor.Direction.REVERSE);
+        bRight.setDirection(DcMotor.Direction.FORWARD);
+        bLeft.setDirection(DcMotor.Direction.REVERSE);
         lift_lol.setDirection(DcMotor.Direction.FORWARD);
         lift_mochi.setDirection(DcMotor.Direction.FORWARD);
         clamp_lol.setDirection(Servo.Direction.FORWARD);
         clamp_mochi.setDirection(Servo.Direction.REVERSE);
 
-        right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         lift_lol.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         lift_mochi.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 
-        right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lift_lol.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lift_mochi.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
@@ -49,8 +59,10 @@ public class ProjectShortage extends Project{
     }
 
     public void Stop(){
-        right.setPower(0);
-        left.setPower(0);
+        fRight.setPower(0);
+        fLeft.setPower(0);
+        bRight.setPower(0);
+        bLeft.setPower(0);
         lift_lol.setPower(0);
         lift_mochi.setPower(0);
         clamp_lol.setPosition(0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
@@ -16,9 +16,23 @@ public class Teleop extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            //teleop goes here (gamepad controls, etc)
-            robot.right.setPower(gamepad1.right_stick_y);
-            robot.left.setPower(-gamepad1.left_stick_y);
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            robot.fLeft.setPower(frontLeftPower);
+            robot.bLeft.setPower(backLeftPower);
+            robot.fRight.setPower(frontRightPower);
+            robot.bRight.setPower(backRightPower);
 
             if (gamepad1.x) {
                 robot.lift_lol.setPower(1);
